6e75fa9ce711e8dee3dd9a52fe04023170fb5b7d
witelokk <death.thunder@yandex.ru>
2023-09-10 00:22:02 +0300

    add --script flag to vshell

---
+++ dz/dz1/vshell/vshell/__main__.py
@@ -0,0 +1,28 @@
+from argparse import ArgumentParser
+
+from .vshell import VShell
+
+
+def main():
+    arg_parser = ArgumentParser("vshell")
+    arg_parser.add_argument("filename", help="zip archive filename")
+    arg_parser.add_argument("--script", help="путь до файла")
+
+    parsed_args = arg_parser.parse_args()
+
+    vshell = VShell(parsed_args.filename)
+
+    if parsed_args.script:
+        try:
+            with open(parsed_args.script) as f:
+                while line := f.readline():
+                    print(f'line "{line.strip()}"')
+                    vshell.execute_command(line.strip())
+        except FileNotFoundError:
+            print(f"Ошибка: файл {parsed_args.script} не существует")
+    else:
+        vshell.loop()
+
+
+if __name__ == "__main__":
+    main()
