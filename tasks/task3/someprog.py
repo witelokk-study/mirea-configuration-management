03364e9aeb036571ab3a614605b468addf0fa17a
witelokk <death.thunder@yandex.ru>
2023-09-11 01:24:14 +0300

    add prac 3 task 3

---
+++ tasks/task3/someprog.py
@@ -0,0 +1,32 @@
+import random
+
+
+def parse_bnf(text):
+    '''
+    Преобразовать текстовую запись БНФ в словарь.
+    '''
+    grammar = {}
+    rules = [line.split('=') for line in text.strip().split('\n')]
+    for name, body in rules:
+        grammar[name.strip()] = [alt.split() for alt in body.split('|')]
+    return grammar
+
+
+def generate_phrase(grammar, start):
+    '''
+    Сгенерировать случайную фразу.
+    '''
+    if start in grammar:
+        seq = random.choice(grammar[start])
+        return ''.join([generate_phrase(grammar, name) for name in seq])
+    return str(start)
+
+
+BNF_TASK3 = '''
+E = bytes
+bytes = byte | byte bytes
+byte = 0 | 1
+'''
+
+for i in range(10):
+    print(generate_phrase(parse_bnf(BNF), 'E'))
