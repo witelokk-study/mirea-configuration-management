fd725964705c48266f4f8518224cf92498495164
witelokk <witelokk@gmail.com>
2023-09-20 03:00:46 +0300

    dz6: clean up

--- dz/dz6/sandboxer/sandboxer/__main__.py
+++ dz/dz6/sandboxer/sandboxer/__main__.py
@@ -19,19 +19,7 @@ def main():
 
     root_gz.seek(0)
     root_cpio_bytes = gzip.open(root_gz).read()
-    # print(f"bytes = {root_cpio_bytes}")
-
-    # extract files from root.cpio
-    temp_dir = mkdtemp()
-    print(temp_dir)
-    subprocess.run(["cpio", "-id", "--no-absolute-filenames"], input=root_cpio_bytes, cwd=temp_dir)
-
-    # copy bash to temp_dir/bin/
-    subprocess.run(["cp", "/usr/bin/bash", path.join(temp_dir, "bin")])
-
-    # pack it back to root.cpio
-    subprocess.run(["find", temp_dir, "|", "cpio", "-o", "-H", "newc", "|", "gzip", "-9", ">", "/rootfs.gz"], input=subprocess.PIPE, cwd=temp_dir)
 
 
-if __name__ == "__main__":  
+if __name__ == "__main__":
     main()
